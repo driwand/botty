1ea175baf7f646322d085cea967c27b35ac5f492
driwand <44057801+driwand@users.noreply.github.com>
2020-04-15 22:18:20 +0100

    ini files

---
+++ bot.js
@@ -0,0 +1,102 @@
+
+'use strict';
+
+const { Client, MessageAttachment } = require('discord.js');
+const fetch = require("node-fetch");
+const Jikan = require('jikan-node');
+const m_genres = require('./genres');
+const embed = require('./embed');
+
+const mal = new Jikan();
+
+const client = new Client();
+const Discord = require('discord.js');
+var i = 0;
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', (message) => {
+    if (message.author == client.user) {
+        return 
+    }
+    if (message.content.startsWith("!")) {
+        manageCommand(message)
+    }
+});  
+
+function manageCommand(msg)
+{
+    let fullCommand = msg.content.substr(1)
+    let cmd = fullCommand.split(' ')[0]
+    let arg = fullCommand.split(' ').splice(1).join(' ');
+
+    if (cmd == "help")
+        handlehelp(msg, cmd, arg)
+    else if (cmd == "anime")
+        handleanime(msg, cmd, arg)
+    else if (cmd == "genres")
+        handlegenres(msg, cmd, arg)
+}
+
+function handlehelp(arg)
+{
+
+}
+
+const get = async (type, title) => {
+    return await mal.search(type, title, "type"); 
+};
+
+const getanimebyid = async (id) => {
+    const res = await fetch(`https://api.jikan.moe/v3/anime/${id}`)
+    return await res.json()
+};
+
+const getgnr = async (id) => {
+    return await mal.findGenre("anime", id, 1); 
+};
+
+function handleanime(msg, cmd, arg)
+{
+    setTimeout(function(){
+        get(cmd, arg)
+        .then(function(data){
+            if (data.results[0].title.toUpperCase() == arg.toUpperCase())
+            {
+                var id = data.results[0].mal_id;
+                console.log(id)
+                getanimebyid(id)
+                    .then(function (result){
+                            msg.channel.send(embed.create_embed(result))
+                        })
+            }
+            else
+                var res = data.results.slice(0, 5);
+        })
+        .catch(err => console.log(err));
+     }, 4000);
+}
+
+function handlegenres(msg, cmd, arg)
+{
+    const args = arg.toUpperCase();
+
+    let id = m_genres.name[args];
+    if (!id)
+    {
+        msg.channel.send(embed.sad_embed());
+        return
+    }
+    setTimeout(function(){
+        getgnr(id)
+        .then(function(data){
+            var index = Math.floor(Math.random() * data.anime.length);
+            msg.channel.send(embed.create_embed(data.anime[index]));
+        })
+        .catch(err => console.log(err));
+    }, 4000);
+}
+
+client.login('Njk5Njk0Njc1NjI3NjA2MDY4.XpYHmg.bS9H_7CdK3hmDtPD_12775zJh3c');
---
+++ embed.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    create_embed ,
+    sad_embed
+}
+
+function create_embed(data)
+{
+    
+    var list_genres = data.genres;
+    console.log(list_genres)
+    const res = list_genres.map(el => el.name).join(', ');
+
+    const embed = new Discord.MessageEmbed()
+    .setColor('#353535')
+    .setTitle(data.title, data.url)
+    .setAuthor('goshujin-sama okaairi nasaaai! I choosed this for you!', 'https://vignette.wikia.nocookie.net/shounen-maid/images/6/6e/Chihiro_infobox.png')
+    .setDescription('did you like it?')
+    .setThumbnail('https://vignette.wikia.nocookie.net/shounen-maid/images/6/6e/Chihiro_infobox.png')
+    .addFields(
+        { name: 'score', value: data.score, inline: true },
+        { name: 'members', value: data.members, inline: true },
+        { name: 'episodes', value: data.episodes, inline: true },
+    )
+    .addField('genres', res, true)
+    .setImage(data.image_url)
+    .setTimestamp()
+    .setFooter('anatano botty <3', 'https://ih0.redbubble.net/image.702318777.9332/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.jpg');
+    return (embed)
+}
+
+function sad_embed()
+{
+    const embed = new Discord.MessageEmbed()
+    .setTitle("goshujin-sama gomee!")
+    .setAuthor("botty", 'https://pbs.twimg.com/profile_images/758277734845915137/Xe8UUiit_400x400.jpg')
+    .setColor('#353535')
+    .setDescription("try again!")
+    .setThumbnail('https://pbs.twimg.com/profile_images/758277734845915137/Xe8UUiit_400x400.jpg')
+    return embed
+}
