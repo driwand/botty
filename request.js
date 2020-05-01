14636e8d68f78a24b624b5804caab6aee38eb4b9
driwand <44057801+driwand@users.noreply.github.com>
2020-05-01 02:09:54 +0000

    fix reaction 	- when looking up for an anime that got multiple results

--- request.js
+++ request.js
@@ -9,7 +9,6 @@ const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
 module.exports = {
     handlehelp,
-    handleanime,
     handlegenres,
     handleseason
 }
@@ -98,93 +97,6 @@ async function handleseason(msg, arg)
             console.log(error);
         }
     });
-}
-
-var check = 0
-
-function handleanime(msg, cmd, arg) {
-    setTimeout(function () {
-        anime.getanime(cmd, arg)
-            .then(function (data) {
-                var res = data.results.slice(0, 5);
-                var i = 0
-                var clicked = 0
-                
-                if (!check)
-                    while (i < 5)
-                    {
-                        if (data.results[i].title.toUpperCase().startsWith(arg.toUpperCase()))
-                            check++;
-                        i++;
-                    }
-                if (check == 1 && data.results[0].title.toUpperCase() == arg.toUpperCase()) {
-                    var id = data.results[0].mal_id;
-                    anime.getanimebyid(id)
-                        .then(function (result) {
-                            msg.channel.send(embed.create_embed(result))
-                        })
-                    check = 0
-                }
-                else {
-                    var usr_id = msg.author.id;
-                    msg.channel.send("1 - " + res[0].title + " \n" +
-                        "2 - " + res[1].title + " \n" +
-                        "3 - " + res[2].title + " \n" +
-                        "4 - " + res[3].title + " \n" +
-                        "5 - " + res[4].title + " \n")
-
-                        .then(function (message) {
-                            message.react('1️⃣')
-                            message.react('2️⃣')
-                            message.react('3️⃣')
-                            message.react('4️⃣')
-                            message.react('5️⃣')
-                        })
-                    
-                    client.on('messageReactionAdd', async (reaction, user) => {
-                        if (reaction.partial)
-                        {
-                            try {
-                                await reaction.fetch()
-                            } catch (error) {
-                                console.log("fetching reaction faild!")
-                                return
-                            }
-                        }
-                        let emoji = reaction.emoji;
-                        if (user.id == usr_id)
-                        {
-                            if (!clicked && emoji.name == '1️⃣') {
-                                handleanime(msg, "anime", res[0].title)
-                                check = 1
-                                clicked = 1;
-                            }
-                            else if (!clicked && emoji.name == '2️⃣') {
-                                handleanime(msg, "anime", res[1].title)
-                                check = 1
-                                clicked = 1;
-                            }
-                            else if (!clicked && emoji.name == '3️⃣') {
-                                handleanime(msg, "anime", res[2].title)
-                                check = 1
-                                clicked = 1;
-                            }
-                            else if (!clicked && emoji.name == '4️⃣') {
-                                handleanime(msg, "anime", res[3].title)
-                                check = 1
-                                clicked = 1;
-                            }
-                            else if (!clicked && emoji.name == '5️⃣') {
-                                handleanime(msg, "anime", res[4].title)
-                                check = 1
-                                clicked = 1;
-                            }
-                        }
-                    });
-                }
-            })
-            .catch(err => console.log(err));
-    }, 4000);
 }
 
 function handlegenres(msg, arg) {
